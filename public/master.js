06bfefabd2e389a94c008720dc842e733b2e0649
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-19 11:50:40 +0200

    Initial electron app structure

---
+++ public/master.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function domReady() {
+    console.log("DOM Loaded!");
+
+    const btn_add_db = document.getElementById("btn_add_db");
+    const btn_del_db = document.getElementById("btn_add_db");
+});
